b699891f22a92e01df2169d6af21e0299bb63253
Egbinola Precious <egbinolaprecious@gmail.com>
2026-09-20 03:18:53 +0100

    Use production backend URL

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ function showCard4MeAlert(message, type = "info", title = "") {
 }
 
 const API_BASE =
-    "https://card4me-backend-1.onrender.com";
+    "https://card4me-backend.onrender.com";
 
 
 /* =====================================================
