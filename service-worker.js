fb22b5558afc982df359e8029086b6aa58b8b6cc
Egbinola Precious <egbinolaprecious@gmail.com>
2026-09-26 16:04:24 +0100

    Refresh service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "card4me-v1";
+const CACHE_NAME = "card4me-v2";
 
 const FILES_TO_CACHE = [
   "/",
@@ -9,9 +9,25 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
   );
 });
 
